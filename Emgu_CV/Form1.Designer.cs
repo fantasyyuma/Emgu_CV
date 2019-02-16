37e2ee288699c9a982f6b7f2b728e7c3f305e3a9
fantasyyuma <31132962+fantasyyuma@users.noreply.github.com>
2019-02-16 14:33:32 +0800

    Initial commit

---
+++ Emgu_CV/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Emgu_CV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 13);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(735, 347);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "重新連接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "擷圖";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(417, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "錄影";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "WebCam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Emgu_CV/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System.Threading;
+using Emgu.CV.CvEnum;
+
+namespace Emgu_CV
+{
+    public partial class Form1 : Form
+    {
+        private Capture _Capture;
+        Image<Bgr,Byte> frame;
+        private bool _captureInProgress = false;
+        Image<Bgr, byte> temp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  private void Form1_Load(object sender, EventArgs e)
+        {
+            _Capture = new Capture(0);
+            Application.Idle += new EventHandler(TimerEventProcessor);
+        }
+        void TimerEventProcessor(object sender, EventArgs e)
+        {
+            frame = _Capture.QueryFrame();
+            imageBox1.Image = frame;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (_Capture == null)
+            {
+                try
+                {
+                    //打開預設的webcam
+                    _Capture = new Capture(0);
+
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            if (_Capture != null)
+            {
+                //frame啟動
+                if (_captureInProgress)
+                {  //stop the capture
+                    _captureInProgress = false;
+                    button1.Text = "開始";
+                    Application.Idle -= TimerEventProcessor;
+                }
+                //frame關閉
+                else
+                {
+                    //start the capture
+                    _captureInProgress = true;
+                    button1.Text = "结束";
+                    Application.Idle += TimerEventProcessor;
+                }
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            _Capture = new Capture(0);
+
+            frame =_Capture.QueryFrame();
+            if (_Capture == null)
+            {
+                try
+                {
+                    //打開預設的webcam
+                    _Capture = new Capture(0);
+
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            else
+            {
+                SaveFileDialog SaveFileDialog1 = new SaveFileDialog
+                {
+                    FileName = DateTime.Now.ToString("yyyy_MMdd_hh_mm_ss"),
+                    Filter = "Image Files(*.bmp)|*.bmp|All files (*.*)|*.*"
+                };
+                if (SaveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    frame.Save(SaveFileDialog1.FileName);
+                }
+            }
+
+
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            _Capture = new Capture();
+            Application.Idle += new EventHandler(TimerEventProcessor);
+            if (_Capture == null)
+            {
+                MessageBox.Show("請重新連接WebCam", "error");
+            }
+            else
+            {
+                temp = _Capture.QueryFrame();
+                SaveFileDialog SaveFileDialog2 = new SaveFileDialog
+                {
+                    FileName = DateTime.Now.ToString("yyyy_MMdd_hh_mm_ss"),
+                    Filter = "Image Files(*.avi)|*.avi|All files (*.*)|*.*"
+                };
+                if (SaveFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show("開始錄製，按ESC結束錄製");
+                }
+                VideoWriter video = new VideoWriter(SaveFileDialog2.FileName, CvInvoke.CV_FOURCC('X', 'V', 'I', 'D'), 60, 640, 480, true);
+                while (temp != null)
+                {
+                    CvInvoke.cvShowImage("camera", temp.Ptr);
+                    temp = _Capture.QueryFrame();
+                    int c = CvInvoke.cvWaitKey(3);
+                    video.WriteFrame<Bgr, byte>(temp);
+                    if (c == 27) break;
+                }
+                video.Dispose();
+                CvInvoke.cvDestroyWindow("camera");
+
+                //錄影完需將影像停止不然會出錯
+                _captureInProgress = false;
+                button1.Text = "開始";
+                Application.Idle -= TimerEventProcessor;
+            }
+
+        }
+       
+}
+}
